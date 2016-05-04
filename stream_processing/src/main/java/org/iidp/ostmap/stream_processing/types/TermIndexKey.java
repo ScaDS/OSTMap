98868f037b68e1c53648bc7719030aff26272866
loewenzahm <scorpnic@gmx.de>
2016-05-04 23:53:13 +0200

    the new key-classes  #31

---
+++ stream_processing/src/main/java/org/iidp/ostmap/stream_processing/types/TermIndexKey.java
@@ -0,0 +1,49 @@
+package org.iidp.ostmap.stream_processing.types;
+
+import java.nio.ByteBuffer;
+
+/**
+ * Contains data for one key of the termIndex-table
+ */
+public class TermIndexKey {
+
+    // constants for source types
+    public static final String SOURCE_TYPE_USER = "user";
+    public static final String SOURCE_TYPE_TEXT = "text";
+
+    // the row in termIndex-table
+    public String term;
+
+    // the column-family in termIndex-table (user or text)
+    public String source;
+
+    // the column-qualifier in termIndex-table which is fk for rawTwitterData
+    public RawTwitterDataKey rawTwitterDataKey;
+
+    // empty constructor
+    public TermIndexKey() {}
+
+    /**
+     * builds a termIndexKey for the given parameter
+     * @param term      token or username
+     * @param source    source-type which defines if user or token
+     * @param rtdKey    the RawTwitterDataKey
+     * @return          the new termIndex object
+     */
+    public static TermIndexKey buildRawTwitterDataKey(String term, String source, RawTwitterDataKey rtdKey) {
+        TermIndexKey key = new TermIndexKey();
+        key.term = term;
+        key.source = source;
+        key.rawTwitterDataKey = rtdKey;
+        return key;
+    }
+
+    public byte[] getTermBytes() {
+        return term.getBytes();
+    }
+
+    public byte[] getSourceBytes() {
+        return source.getBytes();
+    }
+
+}
