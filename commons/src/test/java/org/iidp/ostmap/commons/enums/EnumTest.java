236ba88baf643515f9ea6f17da0fa3868715b8be
Vincent Märkl <vincent@may-maerkl.de>
2016-05-09 16:37:12 +0200

    #35 added tests for accumolo identifier enums

--- commons/src/test/java/org/iidp/ostmap/commons/enums/EnumTest.java
+++ commons/src/test/java/org/iidp/ostmap/commons/enums/EnumTest.java
@@ -23,6 +23,18 @@ public class EnumTest {
         System.out.println((TableIdentifiers.TERM_INDEX));
         String term = TableIdentifiers.TERM_INDEX.toString();
         System.out.println(term);
+        System.out.println((AccumuloIdentifiers.PROPERTY_USER));
+        String user = AccumuloIdentifiers.PROPERTY_USER.toString();
+        System.out.println(user);
+        System.out.println((AccumuloIdentifiers.PROPERTY_INSTANCE));
+        String instance = AccumuloIdentifiers.PROPERTY_INSTANCE.toString();
+        System.out.println(instance);
+        System.out.println((AccumuloIdentifiers.PROPERTY_PASSWORD));
+        String password = AccumuloIdentifiers.PROPERTY_PASSWORD.toString();
+        System.out.println(password);
+        System.out.println((AccumuloIdentifiers.PROPERTY_ZOOKEEPER));
+        String zookeeper = AccumuloIdentifiers.PROPERTY_ZOOKEEPER.toString();
+        System.out.println(zookeeper);
         /**
          *  doesnt work:
          *  String raw = TableIdentifiers.RAW_TWITTER_TABLE:
