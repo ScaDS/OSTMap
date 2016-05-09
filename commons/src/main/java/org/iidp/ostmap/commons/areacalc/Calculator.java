e5b0b20c03139c31a2ae1201ec11c82d25d43eb2
Vincent Märkl <vincent@may-maerkl.de>
2016-05-09 13:51:16 +0200

    Added necessary attributes to configure access to accumulo and a method to set them #36

--- commons/src/main/java/org/iidp/ostmap/commons/areacalc/Calculator.java
+++ commons/src/main/java/org/iidp/ostmap/commons/areacalc/Calculator.java
@@ -1,7 +1,47 @@
 package org.iidp.ostmap.commons.areacalc;
 
+
+import org.apache.hadoop.mapreduce.Job;
+import org.iidp.ostmap.commons.enums.TableIdentifiers;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
 /**
  * This class determines the user with the biggest area defined by his tweets
  */
 public class Calculator {
+    public static final String PROPERTY_INSTANCE = "accumulo.instance";
+    private String accumuloInstanceName;
+    public static final String PROPERTY_USER = "accumulo.user";
+    private String accumuloUser;
+    public static final String PROPERTY_PASSWORD = "accumulo.password";
+    private String accumuloPassword;
+    public static final String PROPERTY_ZOOKEEPER = "accumulo.zookeeper";
+    private String accumuloZookeeper;
+    public static final String inTable = TableIdentifiers.RAW_TWITTER_TABLE.toString();
+    private Job job;
+
+
+
+
+    /**
+     * parses the config file at the given position for the necessary parameters
+     *
+     * @param path
+     * @throws IOException
+     */
+    private void readConfig(String path) throws IOException {
+        Properties props = new Properties();
+        FileInputStream fis = new FileInputStream(path);
+        props.load(fis);
+        accumuloInstanceName = props.getProperty(PROPERTY_INSTANCE);
+        accumuloUser = props.getProperty(PROPERTY_USER);
+        accumuloPassword = props.getProperty(PROPERTY_PASSWORD);
+        accumuloZookeeper = props.getProperty(PROPERTY_ZOOKEEPER);
+    }
+
+
+
 }
