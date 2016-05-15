becdba9cea04bffcb7bacbbbc8e43e110024e70a
Vincent Märkl <vincent@may-maerkl.de>
2016-05-15 13:56:01 +0200

    #36 finished CoordGroupReduce

--- batch_processing/src/main/java/org/iidp/ostmap/batch_processing/areacalc/CoordGroupReduce.java
+++ batch_processing/src/main/java/org/iidp/ostmap/batch_processing/areacalc/CoordGroupReduce.java
@@ -5,12 +5,27 @@ import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.util.Collector;
 
 import java.io.Serializable;
+import java.util.TreeSet;
 
 
 public class CoordGroupReduce implements GroupReduceFunction<Tuple2<String, String>, Tuple2<String, String>>, Serializable {
-
+    private String user = "";
+    private String coords = "";
+    private TreeSet<Tuple2<Double,Double>> coordSet = new TreeSet<Tuple2<Double,Double>>();
     @Override
     public void reduce(Iterable<Tuple2<String, String>> values, Collector<Tuple2<String, /*TODO POJO*/String>> out) throws Exception {
+        for (Tuple2<String,String> entry: values) {
+            if(coords.equals("")){
+                coords += entry.f1;
+            }else{
+                coords += "|" + entry.f1;
+            }
+            user = entry.f0;
+            coordSet.add(new Tuple2<Double,Double>(Double.parseDouble(entry.f1.split(",")[0]),Double.parseDouble(entry.f1.split(",")[1])));
+        }
+        if(coordSet.size() > 2){
+            out.collect(new Tuple2<String,String>(user,coords));
+        }
 
     }
 
