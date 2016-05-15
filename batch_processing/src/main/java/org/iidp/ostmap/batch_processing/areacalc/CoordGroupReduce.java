84623eaed460fb5b891b305a97cd99ea8519ad7c
Vincent Märkl <vincent@may-maerkl.de>
2016-05-15 18:39:38 +0200

    #36 fixed some bugs in the reduce class, added groupreduceclasstest

--- batch_processing/src/main/java/org/iidp/ostmap/batch_processing/areacalc/CoordGroupReduce.java
+++ batch_processing/src/main/java/org/iidp/ostmap/batch_processing/areacalc/CoordGroupReduce.java
@@ -11,7 +11,7 @@ import java.util.TreeSet;
 public class CoordGroupReduce implements GroupReduceFunction<Tuple2<String, String>, Tuple2<String, String>>, Serializable {
     private String user = "";
     private String coords = "";
-    private TreeSet<Tuple2<Double,Double>> coordSet = new TreeSet<Tuple2<Double,Double>>();
+    private TreeSet<String> coordSet = new TreeSet<>();
     @Override
     public void reduce(Iterable<Tuple2<String, String>> values, Collector<Tuple2<String, /*TODO POJO*/String>> out) throws Exception {
         for (Tuple2<String,String> entry: values) {
@@ -21,7 +21,7 @@ public class CoordGroupReduce implements GroupReduceFunction<Tuple2<String, Stri
                 coords += "|" + entry.f1;
             }
             user = entry.f0;
-            coordSet.add(new Tuple2<Double, Double>(Double.parseDouble(entry.f1.split(",")[0]), Double.parseDouble(entry.f1.split(",")[1])));
+            coordSet.add(entry.f1.toString());
         }
         if(coordSet.size() > 2){
             out.collect(new Tuple2<String,String>(user,coords));
