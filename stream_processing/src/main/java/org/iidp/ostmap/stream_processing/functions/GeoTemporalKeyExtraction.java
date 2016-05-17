9dff2ee6efe949b7b17245e8ca35bbcb38309184
loewenzahm <scorpnic@gmx.de>
2016-05-17 13:01:34 +0200

    FlatMap and sink for GeoTemporalIndex

--- stream_processing/src/main/java/org/iidp/ostmap/stream_processing/functions/GeoTemporalKeyExtraction.java
+++ stream_processing/src/main/java/org/iidp/ostmap/stream_processing/functions/GeoTemporalKeyExtraction.java
@@ -1,9 +1,40 @@
 package org.iidp.ostmap.stream_processing.functions;
 
+import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.util.Collector;
+import org.iidp.ostmap.commons.accumulo.geoTemp.GeoTemporalKey;
+import org.iidp.ostmap.stream_processing.types.RawTwitterDataKey;
+import scala.Tuple2;
+
+import java.io.Serializable;
+
 /**
  * Extracts GeoTemporalKey
  */
-public class GeoTemporalKeyExtraction {
+public class GeoTemporalKeyExtraction implements FlatMapFunction<Tuple2<RawTwitterDataKey, String>, Tuple2<RawTwitterDataKey, GeoTemporalKey>>, Serializable {
 
+    /**
+     * empty constructor for serialization (needed by flink)
+     */
+    public GeoTemporalKeyExtraction() {
+    }
+
+    /**
+     * Extract GeoTemporalKey for each tweet
+     * @param input     tuple of rawTwitterDataKey and tweet-json
+     * @param out       tuple of rawTwitterDataKey and GeoTemporalKey
+     * @throws Exception
+     */
+    @Override
+    public void flatMap(Tuple2<RawTwitterDataKey, String> input, Collector<Tuple2<RawTwitterDataKey, GeoTemporalKey>> out) throws Exception {
+
+        // extract geoTempKey
+        GeoTemporalKey geoTempKey = GeoTemporalKey.buildKey(input._2());
+
+        if (geoTempKey.rowBytes!=null && geoTempKey.columQualifier!=null) {
+            out.collect(new Tuple2(input._1(), geoTempKey));
+        }
+
+    }
 
 }
