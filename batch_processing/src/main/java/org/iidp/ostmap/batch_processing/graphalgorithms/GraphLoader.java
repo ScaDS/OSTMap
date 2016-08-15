1e0e4fd9b07899d332d26298b4d072213bf22e37
grimmer <grimmer@VM-Lubuntu-16.04>
2016-08-15 11:04:40 +0200

    further preparations for graph algorithms, interface definition

--- batch_processing/src/main/java/org/iidp/ostmap/batch_processing/graphalgorithms/GraphLoader.java
+++ batch_processing/src/main/java/org/iidp/ostmap/batch_processing/graphalgorithms/GraphLoader.java
@@ -1,7 +1,22 @@
 package org.iidp.ostmap.batch_processing.graphalgorithms;
 
+import org.apache.flink.graph.Graph;
+import org.iidp.ostmap.batch_processing.graphalgorithms.datastructures.UserEdgeValues;
+import org.iidp.ostmap.batch_processing.graphalgorithms.datastructures.UserNodeValues;
+
 public class GraphLoader {
 
 
+    /**
+     * builds a user graph from tweets in json format
+     * an user represents an vertex
+     * an edge A->B is created if an user A mentions another user B
+     * @param path path to folder containing json files to load as user graph
+     * @return
+     */
+    public Graph<String, UserNodeValues, UserEdgeValues> getUserGraphFromFiles(String path) {
+
+        return null;
+    }
 
 }
---
+++ batch_processing/src/main/java/org/iidp/ostmap/batch_processing/graphalgorithms/datastructures/UserNodeValues.java
@@ -0,0 +1,11 @@
+package org.iidp.ostmap.batch_processing.graphalgorithms.datastructures;
+
+/**
+ *
+ */
+public class UserNodeValues {
+
+    public String userName;
+    //public String has
+
+}
