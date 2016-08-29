14f9435a7ad15f1cc31a8b087145e18168328ba5
Matthias Kricke <kricke@informatik.uni-leipzig.de>
2016-08-29 15:02:00 +0200

    first connection analysis

--- batch_processing/src/main/java/org/iidp/ostmap/batch_processing/graphprocessing/algorithms/ConnectionAnalysis.java
+++ batch_processing/src/main/java/org/iidp/ostmap/batch_processing/graphprocessing/algorithms/ConnectionAnalysis.java
@@ -1,19 +1,29 @@
 package org.iidp.ostmap.batch_processing.graphprocessing.algorithms;
 
 import org.apache.commons.cli.MissingArgumentException;
+import org.apache.flink.api.common.functions.GroupCombineFunction;
+import org.apache.flink.api.common.operators.Order;
 import org.apache.flink.api.java.DataSet;
+
+import org.apache.flink.api.java.operators.GroupCombineOperator;
+import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.graph.Graph;
 import org.apache.flink.graph.Vertex;
 import org.apache.flink.graph.library.ConnectedComponents;
+import org.apache.flink.util.Collector;
 import org.iidp.ostmap.batch_processing.graphprocessing.GraphLoader;
 import org.iidp.ostmap.batch_processing.graphprocessing.datastructures.UserEdgeValues;
 import org.iidp.ostmap.batch_processing.graphprocessing.datastructures.UserNodeValues;
+import org.slf4j.Logger;
+import static org.slf4j.LoggerFactory.getLogger;
+
 
 /**
  * This class is used to get a brief overview of the graph by creating a summary with the help of the weakly connected
  * component analysis of gelly.
  */
 public class ConnectionAnalysis {
+    private static final Logger logger = getLogger(ConnectionAnalysis.class);
     private static final Integer MAX_ITERATIONS = 100;
 
 
@@ -25,8 +35,45 @@ public class ConnectionAnalysis {
         ConnectedComponents<String, UserNodeValues, UserEdgeValues> cc = new ConnectedComponents<>(MAX_ITERATIONS);
         DataSet<Vertex<String, UserNodeValues>> ccResult = cc.run(graph);
 
-        ccResult.groupBy(2).sum(1);
-        ccResult.groupBy(2).sum(2);
+        //***************************************************************************************
+        // count number of weakly connected components
+        //***************************************************************************************
+        logger.info("Number of weakly connected components: ");
+        ccResult.groupBy("V").combineGroup(new GroupCombineFunction<Vertex<String,UserNodeValues>, Long>() {
+            @Override
+            public void combine(Iterable<Vertex<String, UserNodeValues>> values, Collector<Long> out) throws Exception {
+                out.collect(1L);
+            }
+        }).sum(0).print();
+
+
+
+
+        //***************************************************************************************
+        // print sizes of weakly connected components, sort desc
+        //***************************************************************************************
+        logger.info("Size of weakly connected components (sorted, desc): ");
+        DataSet<Tuple2<UserNodeValues, Long>> countMap = ccResult.groupBy("V")
+                .combineGroup(new GroupCombineFunction<Vertex<String, UserNodeValues>, Tuple2<UserNodeValues, Long>>() {
+            @Override
+            public void combine(Iterable<Vertex<String, UserNodeValues>> values, Collector<Tuple2<UserNodeValues, Long>> out) throws Exception {
+                long componentSize = 0;
+                UserNodeValues compIdentifier = null;
+                for (Vertex<String, UserNodeValues> vertex : values) {
+                    componentSize++;
+
+                    if (compIdentifier != null && !compIdentifier.equals(vertex.getValue())) {
+                        throw new IllegalArgumentException(compIdentifier + " should be equal to " + vertex.getValue());
+                    } else {
+                        compIdentifier = vertex.getValue();
+                    }
+                }
+                out.collect(new Tuple2<>(compIdentifier, componentSize));
+            }
+        });
+        countMap.sortPartition("T1", Order.DESCENDING).first(10).print();
+
+
     }
 
 
