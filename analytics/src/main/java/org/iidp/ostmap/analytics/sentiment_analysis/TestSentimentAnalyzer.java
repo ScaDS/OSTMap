a80359c7529f438888b996d88957397b8751977a
Alexander Heimann <ah28vyto@studserv.uni-leipzig.de>
2018-06-11 16:00:20 +0200

    [sentiment-analysis] trail and error

---
+++ analytics/src/main/java/org/iidp/ostmap/analytics/sentiment_analysis/TestSentimentAnalyzer.java
@@ -0,0 +1,44 @@
+package org.iidp.ostmap.analytics.sentiment_analysis;
+
+import org.apache.accumulo.core.client.*;
+import org.apache.accumulo.core.data.Key;
+import org.apache.accumulo.core.data.Value;
+import org.iidp.ostmap.commons.accumulo.AccumuloService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.apache.accumulo.core.data.Range;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class TestSentimentAnalyzer {
+
+    static Logger log = LoggerFactory.getLogger(TestSentimentAnalyzer.class);
+
+    private AccumuloService accumuloService;
+
+    public TestSentimentAnalyzer() throws IOException, AccumuloSecurityException, AccumuloException, TableNotFoundException {
+        this.accumuloService = new AccumuloService();
+        String configPath = MainController.configFilePath;
+
+        this.accumuloService.readConfig(configPath);
+
+        List<Range> rangeList = new ArrayList<>();
+
+        rangeList = new ArrayList<>();
+
+//        for(String field:fieldArray){
+//            // get all results from tokenIndex to the list
+//            Scanner termIndexScanner = accumuloService.getTermIndexScanner(token,field);
+//            for (Map.Entry<Key, Value> termIndexEntry : termIndexScanner) {
+//                rawKeys.add(new Range(termIndexEntry.getKey().getColumnQualifier()));
+//            }
+//            termIndexScanner.close();
+//        }
+
+        BatchScanner batchScanner = this.accumuloService.getRawDataBatchScanner(rangeList);
+    }
+}
